0e4d3cfa177d04cc7e95136757c96cd40acb4d9b
Obinna Ogbonna <oogbonna@gidimobile.com>
2020-09-04 07:52:32 +0100

    implement box dragging using fsm

--- src/index.js
+++ src/index.js
@@ -3,57 +3,109 @@ import { createMachine, assign, interpret } from 'xstate';
 const elBox = document.querySelector('#box');
 const elBody = document.body;
 
+const assignPoint = assign({
+    px: (context, event) => event.clientX,
+    py: (context, event) => event.clientY
+});
+
+const assignPosition = assign({
+    x: (context, event) => {
+        return context.x + context.dx;
+    },
+    y: (context, event) => {
+        return context.y + context.dy
+    },
+    dx: 0,
+    dy: 0,
+    px: 0,
+    py: 0
+})
+
+const assignDelta = assign({
+    dx: (context, event) => {
+        return event.clientX - context.px;
+    },
+    dy: (context, event) => {
+        return event.clientY - context.py;
+    }
+})
+
+
+const showDelta = (context) => {
+    elBox.dataset.delta = `delta: ${context.dx}, ${context.dy}`;
+}
+
+const resetPosition = assign({
+    dx: 0,
+    dy: 0,
+    px: 0,
+    py: 0
+})
+
 const machine = createMachine({
-  initial: 'idle',
-  // Set the initial context
-  // Clue: {
-  //   x: 0,
-  //   y: 0,
-  //   dx: 0,
-  //   dy: 0,
-  //   px: 0,
-  //   py: 0,
-  // }
-  // context: ...,
-  states: {
-    idle: {
-      on: {
-        mousedown: {
-          // Assign the point
-          // ...
-          target: 'dragging',
-        },
-      },
+    initial: 'idle',
+    // Set the initial context
+    // Clue: {
+    //   x: 0,
+    //   y: 0,
+    //   dx: 0,
+    //   dy: 0,
+    //   px: 0,
+    //   py: 0,
+    // }
+    // context: ...,
+    context: {
+        x: 0,
+        y: 0,
+        dx: 0,
+        dy: 0,
+        px: 0,
+        py: 0
     },
-    dragging: {
-      on: {
-        mousemove: {
-          // Assign the delta
-          // ...
-          // (no target!)
+    states: {
+        idle: {
+            on: {
+                mousedown: {
+                    // Assign the point
+                    // ...
+                    actions: assignPoint,
+                    target: 'dragging',
+                },
+            },
         },
-        mouseup: {
-          // Assign the position
-          target: 'idle',
+        dragging: {
+            on: {
+                mousemove: {
+                    // Assign the delta
+                    // ...
+                    // (no target!)
+                    actions: [assignDelta, showDelta]
+                },
+                mouseup: {
+                    // Assign the position
+                    actions: assignPosition,
+                    target: 'idle',
+                },
+                'keyup.escape': {
+                    target: 'idle',
+                    actions: resetPosition
+                }
+            },
         },
-      },
     },
-  },
 });
 
 const service = interpret(machine);
 
 service.onTransition((state) => {
-  if (state.changed) {
-    console.log(state.context);
+    if (state.changed) {
+        elBox.dataset.state = state.value;
 
-    elBox.dataset.state = state.value;
-
-    elBox.style.setProperty('--dx', state.context.dx);
-    elBox.style.setProperty('--dy', state.context.dy);
-    elBox.style.setProperty('--x', state.context.x);
-    elBox.style.setProperty('--y', state.context.y);
-  }
+        elBox.style.setProperty('--dx', state.context.dx);
+        elBox.style.setProperty('--dy', state.context.dy);
+        elBox.style.setProperty('--x', state.context.x);
+        elBox.style.setProperty('--y', state.context.y);
+    }
 });
 
 service.start();
@@ -62,3 +114,21 @@ service.start();
 // - mousedown on elBox
 // - mousemove on elBody
 // - mouseup on elBody
+
+elBox.addEventListener('mousedown', (event) => {
+    service.send(event);
+})
+
+elBody.addEventListener('mousemove', (event) => {
+    service.send(event);
+})
+
+elBody.addEventListener('mouseup', (event) => {
+    service.send(event);
+})
+
+elBody.addEventListener('keyup', (e) => {
+    if (e.key === 'Escape') {
+        service.send('keyup.escape');
+    }
+})
